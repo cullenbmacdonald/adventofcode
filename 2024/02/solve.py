40950e3b3305b918b9436d5b09c7aced2b5553e0
Cullen MacDonald <cullenbmacdonald@gmail.com>
2024-12-02 07:49:34 -0500

    solved day 2 question 1

--- 2024/02/solve.py
+++ 2024/02/solve.py
@@ -1,34 +1,54 @@
-import functools, operator
-from collections import defaultdict
-from typing import Tuple, List, Dict
+import functools
+from typing import List
 
-def __add_to_arr(arr, part):
-    arr.append(int(part.strip()))
+INCREASING = 1
+DECREASING = -1
+UNKNOWN = 0 
 
-def __process_right_list(right: List[int]) -> Dict[int, int]:
-    counts = defaultdict(int)
-    for location_id in right:
-        counts[location_id] = counts[location_id] + 1
-    return counts
+class WTFError(Exception):
+    pass
 
-# Doesnt need to be sorted for this one
-def execute(left: List[int], right: List[int]) -> int:
-    right_counts = __process_right_list(right)
-    similarity = [l * right_counts[l] for l in left]
-    return functools.reduce(operator.add, similarity)
+def get_direction(first: int, second: int) -> int:
+    if first < second:
+        return INCREASING
+    elif first > second:
+        return DECREASING
+    elif first == second:
+        return UNKNOWN
+    else:
+        raise WTFError()
 
-def process_file_input() -> Tuple[List[int], List[int]]:
+def increment_safe(safe_count: int, line: str) -> int:
+    report = [int(l) for l in line.split(" ")]
+    prev_direction = None
+    for idx, num in enumerate(report):
+        if idx == 0:
+            continue
+
+        prev_num = report[idx - 1]
+        curr_direction = get_direction(prev_num, num)
+        if prev_direction == None:
+            prev_direction = curr_direction
+
+        distance = abs(prev_num - num)
+
+        if distance <= 0 or distance > 3:
+            return safe_count
+
+        if prev_direction != curr_direction:
+            return safe_count
+
+    return safe_count + 1
+
+
+def execute() -> List[List[int]]:
     left = []
-    right = []
     with open("input.txt") as file:
-        for line in file:
-            split_up = line.split("   ")
-            __add_to_arr(left, split_up[0])
-            __add_to_arr(right, split_up[1])
+        safe_count = functools.reduce(increment_safe, file, 0)
 
-    return (left, right)
+    return (safe_count, None)
 
 if __name__ == "__main__":
-    file_input = process_file_input()
-    solution = execute(*file_input)
-    print(solution)
+    solution_01, solution_02 = execute()
+    print(solution_01)
+    #print(solution_02)
